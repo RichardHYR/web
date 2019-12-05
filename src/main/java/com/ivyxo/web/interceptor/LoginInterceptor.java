df3edfb8dbf46a6831472f15259e692cf7de15ca
RichardHYR <richard_hyr@163.com>
2019-12-05 16:07:26 +0800

    测试

--- src/main/java/com/ivyxo/web/interceptor/LoginInterceptor.java
+++ src/main/java/com/ivyxo/web/interceptor/LoginInterceptor.java
@@ -70,6 +70,7 @@ public class LoginInterceptor implements HandlerInterceptor{
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
+
     }
 
     @Override
